1eaa668b8585cf85cd17338db4360da6be445091
renraelreve <123564916+renraelreve@users.noreply.github.com>
2024-03-02 21:35:14 +0800

    done assignment for 1.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-function sum(n1, n2){return n1 + n2;} const x = sum(1,1); console.log(x);
+function sum(n1, n2) {
+  return n1 + n2;
+}
+
+const userInput1 = prompt("Please enter a number:");
+const userInput2 = prompt("Please enter a number:");
+
+const x = sum(Number(userInput1), Number(userInput2));
+console.log(x);
